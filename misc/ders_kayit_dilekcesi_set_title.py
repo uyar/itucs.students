026899d56db5874bfca039aa6aebe85f1d441698
H. Turgut Uyar <uyar@itu.edu.tr>
2017-08-31 20:17:22 +0300

    Update title from student no and course code

--- misc/ders_kayit_dilekcesi_set_title.py
+++
@@ -1,3 +0,0 @@
-context.setTitle(context.ogrenci_no + ' - ' + context.ad_soyad + ' - ' + context.ders_kod_onek + context.ders_kod)
-context.reindexObject()
-
--- src/itucs/students/content.py
+++ src/itucs/students/content.py
@@ -4,3 +4,12 @@ from plone.dexterity.content import Item
 class EnrollmentRequest(Item):
 
     __ac_local_roles_block__ = True
+
+
+def updateTitle(enrollment_request, event):
+    student_no = enrollment_request.student_no
+    course_prefix = enrollment_request.course_prefix
+    course_no = enrollment_request.course_no
+    title = u"%s - %s%s" % (student_no, course_prefix, course_no)
+    enrollment_request.title = title
+    enrollment_request.setTitle(title)
